7ec8697718aca16b79d725a95617b190fd94fea6
Berend Wouters <online@visionsandviews.net>
2015-07-04 13:26:49 +0200

    Authentication added

--- Python/client.py
+++ Python/client.py
@@ -1,5 +1,26 @@
-import http.client;
-conn = http.client.HTTPConnection("http://space.visionsandviews.net/spaceapi/admin");
-conn.request("GET", "/");
-response = conn.getresponse();
-print(response.status, response.reason);
+import json, requests
+
+url = 'http://space.visionsandviews.net/spaceapi/admin'
+openUrl = 'http://space.visionsandviews.net/spaceapi/admin/open'
+params = dict()
+headers = {'user-agent': 'my-app/0.0.1', 'AUTH' : ''}
+resp = requests.get(url=url, params=params)
+data = json.loads(resp.text)
+print(data)
+print("\n")
+print("Open space")
+openUrl = 'http://space.visionsandviews.net/spaceapi/admin/open'
+params = dict()
+
+resp = requests.get(url=openUrl, params=params, headers=headers)
+
+data = json.loads(resp.text)
+print(data)
+print("\n")
+print("Close space")
+closeUrl = 'http://space.visionsandviews.net/spaceapi/admin/close'
+params = dict()
+
+resp = requests.get(url=closeUrl, params=params, headers=headers)
+data = json.loads(resp.text)
+print(data)
