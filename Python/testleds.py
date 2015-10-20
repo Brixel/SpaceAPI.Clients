068020e06fe2261fc2fd51cfe4860f254041f800
Berend Wouters <berend.wouters@ugentec.com>
2015-10-20 22:07:55 +0200

    Updates the pi to use PWM for the buttons to show up

--- Python/testleds.py
+++ Python/testleds.py
@@ -1,60 +1,62 @@
-#!/usr/bin/env python2.7
-# script by Alex Eames http://RasPi.tv
-# http://RasPi.tv/how-to-use-interrupts-with-python-on-the-raspberry-pi-and-rpi-gpio-part-3
 import RPi.GPIO as GPIO
-import time;
+import time
 GPIO.setmode(GPIO.BCM)
 
 # GPIO 23 & 17 set up as inputs, pulled up to avoid false detection.
 # Both ports are wired to connect to GND on button press.
 # So we'll be setting up falling edge detection for both
-GPIO.setup(17, GPIO.IN, pull_up_down=GPIO.PUD_UP) #button
-GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_UP) #button
-GPIO.setup(22, GPIO.OUT) #led 
-GPIO.setup(23, GPIO.OUT) #led
+GPIO.setup(17, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
-GPIO.output(22,1)
-GPIO.output(23,1)
 
-time.sleep(5)
-GPIO.output(22,0)
-GPIO.output(23,0)
-# GPIO 24 set up as an input, pulled down, connected to 3V3 on button press
-#GPIO.setup(24, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(22, GPIO.OUT) #led red
+GPIO.setup(23, GPIO.OUT) #led green
+
+p_red = GPIO.PWM(22, 0.5)
+p_green = GPIO.PWM(23, 0.5)
+p_red.start(100)
+p_green.start(80)
+
+p_red.ChangeFrequency(100)
+p_green.ChangeFrequency(100)
+
 
 # now we'll define two threaded callback functions
 # these will run in another thread when our events are detected
-def openPressed(channel):
+def close_callback(channel):
     print "falling edge detected on 17"
-    GPIO.output(22, 1)
-    GPIO.output(23, 0)
-    raw_input("Press Enter when ready\n>")
+    p_red.start(40)
+    #p_green.ChangeDutyCycle(0)
+    p_green.stop()
+    execfile("close.py")
+    print "Closed"
+    p_red.ChangeDutyCycle(100)
+    p_green.stop()
+
+def open_callback(channel):
+    print "falling edge detected on 23"
+    p_green.start(30)
+    p_red.stop()
+    execfile("open.py")
+    print "Opened"
+    p_green.ChangeDutyCycle(100)
 
 
-def closePressed(channel):
-    print "FALLINing edge detected on 18"
-    GPIO.output(23, 1)
-    GPIO.output(22, 0)
-    raw_input("Press Enter when ready\n>")
-
-
-# when a falling edge is detected on port 17, regardless of whatever 
-# else is happening in the program, the function openPressed will be run
-GPIO.add_event_detect(17, GPIO.RISING, callback=closePressed, bouncetime=200)
-#GPIO.add_event_callback(17, closePressed)
-
-# when a falling edge is detected on port 23, regardless of whatever 
-# else is happening in the program, the function closePressed will be run
-# 'bouncetime=300' includes the bounce control written into interrupts2a.py
-GPIO.add_event_detect(18, GPIO.RISING, callback=openPressed, bouncetime=200)	
 #raw_input("Press Enter when ready\n>")
 
+# when a falling edge is detected on port 17, regardless of whatever
+# else is happening in the program, the function my_callback will be run
+GPIO.add_event_detect(17, GPIO.RISING, callback=close_callback, bouncetime=300)
 
+# when a falling edge is detected on port 23, regardless of whatever
+# else is happening in the program, the function my_callback2 will be run
+# 'bouncetime=300' includes the bounce control written into interrupts2a.py
+GPIO.add_event_detect(18, GPIO.RISING, callback=open_callback, bouncetime=300)
 
 try:
-	while True: 
-	pass 
+        while True:
+                 pass
+
+
 except KeyboardInterrupt:
-    GPIO.cleanup()       # clean up GPIO on CTRL+C exit
-GPIO.cleanup()           # clean up GPIO on normal exit
-	
+        GPIO.cleanup()           # clean up GPIO on normal exit
